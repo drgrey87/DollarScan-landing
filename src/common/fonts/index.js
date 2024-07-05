0106600d4cf968acf8d623583e19a6f043a28202
Sergey Kucheyko <dr.grey_101by@inbox.ru>
2024-07-05 19:44:29 +0300

    added site

---
+++ src/common/fonts/index.js
@@ -0,0 +1,7 @@
+import * as MontserratBold from './Montserrat-Bold.ttf';
+import * as MontserratMedium from './Montserrat-Medium.ttf';
+import * as MontserratRegular from './Montserrat-Regular.ttf';
+import * as MontserratSemiBold from './Montserrat-SemiBold.ttf';
+import * as RobotoCondensedRegular from './RobotoCondensed-Regular.ttf';
+
+export {MontserratBold, MontserratMedium, MontserratRegular, MontserratSemiBold, RobotoCondensedRegular};
